b851d083120d12ed9c232ef8a7a3b3fcca141db1
khannh <kn213742@gmail.com>
2026-02-25 19:39:44 +0700

    25-2 update

---
+++ src/main/java/vn/hoidanit/springsieutoc/controller/UserController.java
@@ -0,0 +1,44 @@
+package vn.hoidanit.springsieutoc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import vn.hoidanit.springsieutoc.service.UserService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+// @RestController
+// public class UserController {
+
+//     private UserService userService;
+
+//     public UserController(UserService userService) {
+//         this.userService = userService;
+//     }
+
+//     @GetMapping("/")
+//     public String getHomePage() {
+//         return this.userService.handlHello();
+//     }
+// }
+@Controller
+public class UserController {
+
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @RequestMapping("/")
+    public String getHomePage(Model model) {
+        String test = this.userService.handlHello();
+        model.addAttribute("khanh", test);
+        model.addAttribute("kabeo", "from controller with model");
+        return "hello";
+    }
+
+}
---
+++ src/main/java/vn/hoidanit/springsieutoc/service/UserService.java
@@ -0,0 +1,10 @@
+package vn.hoidanit.springsieutoc.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    public String handlHello() {
+        return "Hello from service :>";
+    }
+}
